44ecf75fa0622b1864f212ab1ee398e9987ebd39
kmeshkov <meshkob@rambler.ru>
2020-03-02 09:50:37 +0700

    added more test data updated readme

--- test_data_init.sh
+++ test_data_init.sh
@@ -9,13 +9,32 @@ transactionApiURL=http://localhost:$port/api/transaction
 curl -H "Content-Type: application/json" -d '{"firstName":"Ilya","secondName":"Popov"}' $userApiURL
 curl -H "Content-Type: application/json" -d '{"firstName":"Magomed","secondName":"Ozdolev"}' $userApiURL
 curl -H "Content-Type: application/json" -d '{"firstName":"Marina","secondName":"Irina"}' $userApiURL
+curl -H "Content-Type: application/json" -d '{"firstName":"Olga","secondName":"Kokorina"}' $userApiURL
+curl -H "Content-Type: application/json" -d '{"firstName":"New","secondName":"User"}' $userApiURL
 
 curl -H "Content-Type: application/json" -d '{"ownerUid":1}' $accountApiURL
 curl -H "Content-Type: application/json" -d '{"ownerUid":2}' $accountApiURL
 curl -H "Content-Type: application/json" -d '{"ownerUid":3}' $accountApiURL
+curl -H "Content-Type: application/json" -d '{"ownerUid":4}' $accountApiURL
+curl -H "Content-Type: application/json" -d '{"ownerUid":5}' $accountApiURL
 
 curl -H "Content-Type: application/json" -d '{"toAccount":1, "amount":1000.1}' $transactionApiURL/deposit
 curl -H "Content-Type: application/json" -d '{"toAccount":2,"fromAccount":1, "amount":1000}' $transactionApiURL/transfer
-
+curl $accountApiURL/1 # should have balance 0.1
 curl $accountApiURL/2 # should have balance 1000
+
+curl -H "Content-Type: application/json" -d '{"toAccount":3, "amount":10000}' $transactionApiURL/deposit
+curl $accountApiURL/3 # should have balance 10000
+
+curl -H "Content-Type: application/json" -d '{"toAccount":4, "amount":-1 }' $transactionApiURL/deposit
+curl $accountApiURL/4 # should have balance 0
+
+curl -H "Content-Type: application/json" -d '{"toAccount":5, "amount":1000}' $transactionApiURL/deposit
+curl $accountApiURL/5 # should have balance 1000
+curl -H "Content-Type: application/json" -d '{"toAccount":5, "amount":1000.1}' $transactionApiURL/withdraw
+curl $accountApiURL/5 # should have balance 1000
+curl -H "Content-Type: application/json" -d '{"toAccount":5, "amount":1000}' $transactionApiURL/withdraw
+curl $accountApiURL/5 # should have balance 0
+
+
 
